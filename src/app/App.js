a6dedf6f26de6a34093dafce89d4114bd64acab5
Hovik Geodakyan <hovik.geodakyan@gmail.com>
2019-12-15 01:34:58 +0400

    background

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Buttons from './Buttons';
+import Background from '../static/background.jpg';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    height: '100vh'
+    height: '100vh',
+    backgroundImage: `url(${Background})`,
+  },
+  backgroundOverlay: {
+    position: 'absolute',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: '100%',
+    height: '100%',
   },
   footerContainer: {
     // background: '#d7d7d7',
@@ -19,6 +27,7 @@ export default function App() {
 
   return (
     <Grid container direction='column' justify='flex-end' className={classes.root}>
+      <div className={classes.backgroundOverlay}/>
       <Grid item className={classes.footerContainer}>
         <Buttons/>
       </Grid>
